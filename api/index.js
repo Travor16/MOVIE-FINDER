b5cbe06af9ba39317affb0d7a5af94011f17ae11
Micheal <travormicheal7@gmail.com>
2026-07-13 15:45:40 +0300

    Remove try/catch from handler

--- api/index.js
+++ api/index.js
@@ -1,16 +1,10 @@
-// Test if module loads at all
+// Super simple handler - no try/catch, just send response
 console.log('[MODULE] Module execution started');
 
-// Most basic possible handler
 module.exports = async (req, res) => {
   console.log('[HANDLER] Request received:', req.method, req.url);
-  try {
-    res.status(200).send('Hello from basic handler!');
-    console.log('[HANDLER] Response sent');
-  } catch (err) {
-    console.log('[HANDLER] Error sending response:', err.message);
-    res.status(500).send('Internal Server Error');
-  }
+  res.status(200).send('Hello from basic handler!');
+  console.log('[HANDLER] Response sent');
 };
 
 console.log('[MODULE] Module execution completed');
