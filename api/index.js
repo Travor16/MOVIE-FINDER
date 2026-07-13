a8c001d9a12861202294243421a4ebb013fd76fe
Micheal <travormicheal7@gmail.com>
2026-07-13 15:40:36 +0300

    Test: Add module load and handler logs to see if code runs

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
-// Most basic possible handler - plain Node.js
+// Test if module loads at all
+console.log('[MODULE] Module execution started');
+
+// Most basic possible handler
 module.exports = async (req, res) => {
-  console.log('[handler] Request received:', req.method, req.url);
-  res.status(200).send('Hello from basic handler!');
+  console.log('[HANDLER] Request received:', req.method, req.url);
+  try {
+    res.status(200).send('Hello from basic handler!');
+    console.log('[HANDLER] Response sent');
+  } catch (err) {
+    console.log('[HANDLER] Error sending response:', err.message);
+    res.status(500).send('Internal Server Error');
+  }
 };
+
+console.log('[MODULE] Module execution completed');
