fba4d12d805c43c1926a16723413dfd81dd98dfe
Micheal <travormicheal7@gmail.com>
2026-07-13 16:00:48 +0300

    Add error handling to handler to capture any exceptions

--- api/index.js
+++ api/index.js
@@ -1,11 +1,17 @@
-// Using correct Node.js http response methods
+// Handler with error catching to see what's going wrong
 console.log('[MODULE] Module execution started');
 
 module.exports = (req, res) => {
   console.log('[HANDLER] Request received:', req.method, req.url);
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('OK');
-  console.log('[HANDLER] Response sent');
+  try {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('OK');
+    console.log('[HANDLER] Response sent');
+  } catch (err) {
+    console.log('[HANDLER] Error:', err.message);
+    res.writeHead(500, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ error: err.message, stack: err.stack }));
+  }
 };
 
 console.log('[MODULE] Module execution completed');
