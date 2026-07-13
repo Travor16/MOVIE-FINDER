5c7e52eee8810c7dffdab5fbc05ec1ab82f17826
Micheal <travormicheal7@gmail.com>
2026-07-13 15:54:09 +0300

    Use correct Node.js http response methods (writeHead/end)

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
-// Minimal response - just send text without status
+// Using correct Node.js http response methods
 console.log('[MODULE] Module execution started');
 
 module.exports = (req, res) => {
   console.log('[HANDLER] Request received:', req.method, req.url);
-  res.send('OK');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('OK');
   console.log('[HANDLER] Response sent');
 };
 
